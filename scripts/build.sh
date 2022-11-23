d5ce6ead01906a5fe469e46f96eb756a11a6dd0b
TwiN <twin@linux.com>
2022-11-22 19:15:30 -0500

    build: Fix path in build script
    Fixes #4

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,22 +9,22 @@ APPLICATION_NAME="whois"
 mkdir bin
 rm -rf bin/${APPLICATION_NAME}-*.zip
 
-env GOOS=darwin GOARCH=amd64 go build -mod vendor -o ${APPLICATION_NAME}
+env GOOS=darwin GOARCH=amd64 go build -o ${APPLICATION_NAME} ./cmd/whois/main.go
 chmod +x ${APPLICATION_NAME}
 zip bin/${APPLICATION_NAME}-darwin-amd64.zip ${APPLICATION_NAME} -m
 
-env GOOS=darwin GOARCH=arm64 go build -mod vendor -o ${APPLICATION_NAME}
+env GOOS=darwin GOARCH=arm64 go build -o ${APPLICATION_NAME} ./cmd/whois/main.go
 chmod +x ${APPLICATION_NAME}
 zip bin/${APPLICATION_NAME}-darwin-arm64.zip ${APPLICATION_NAME} -m
 
-env GOOS=linux GOARCH=amd64 go build -mod vendor -o ${APPLICATION_NAME}
+env GOOS=linux GOARCH=amd64 go build -o ${APPLICATION_NAME} ./cmd/whois/main.go
 chmod +x ${APPLICATION_NAME}
 zip bin/${APPLICATION_NAME}-linux-amd64.zip ${APPLICATION_NAME} -m
 
-env GOOS=linux GOARCH=arm64 go build -mod vendor -o ${APPLICATION_NAME}
+env GOOS=linux GOARCH=arm64 go build -o ${APPLICATION_NAME} ./cmd/whois/main.go
 chmod +x ${APPLICATION_NAME}
 zip bin/${APPLICATION_NAME}-linux-arm64.zip ${APPLICATION_NAME} -m
 
-env GOOS=windows GOARCH=amd64 go build -mod vendor -o ${APPLICATION_NAME}.exe
+env GOOS=windows GOARCH=amd64 go build -o ${APPLICATION_NAME}.exe ./cmd/whois/main.go
 chmod +x ${APPLICATION_NAME}.exe
 zip bin/${APPLICATION_NAME}-windows-amd64.zip ${APPLICATION_NAME}.exe -m
